d8985ea530ab84b1701615870044cfb00e79c569
christianstender <miniporse@gmail.com>
2023-02-17 18:13:01 +0100

    Javascript fixed
    Changed to queryselectorAll

--- main.js
+++ main.js
@@ -38,43 +38,44 @@ document.addEventListener('click', (event) => {
   }
 });
 
-// Get the trigger-modal element
-const triggerModal = document.querySelector('.trigger-modal');
+// Get all the trigger-modal elements
+const triggerModals = document.querySelectorAll('.trigger-modal');
 
-// Add a click event listener to the trigger-modal element
-triggerModal.addEventListener('click', () => {
-  // Get the video id from the data-video-id attribute
-  const videoId = triggerModal.getAttribute('data-video-id');
+// Loop through each trigger-modal element
+triggerModals.forEach(triggerModal => {
+  // Add a click event listener to the trigger-modal element
+  triggerModal.addEventListener('click', () => {
+    // Get the video id from the data-video-id attribute
+    const videoId = triggerModal.getAttribute('data-video-id');
 
-  // Create the YouTube video URL with the video id
-  const videoUrl = `https://www.youtube.com/embed/${videoId}?controls=0&modestbranding=1&rel=0&showinfo=0`;
+    // Create the YouTube video URL with the video id
+    const videoUrl = `https://www.youtube.com/embed/${videoId}?controls=0&modestbranding=1&rel=0&showinfo=0`;
 
-  // Create a div to hold the iframe
-  const modal = document.createElement('div');
-  modal.classList.add('fullscreen-modal');
+    // Create a div to hold the iframe
+    const modal = document.createElement('div');
+    modal.classList.add('fullscreen-modal');
 
-  // Create the close button
-  const closeButton = document.createElement('button');
-  closeButton.classList.add('close');
-  closeButton.innerHTML = 'X';
+    // Create the close button
+    const closeButton = document.createElement('button');
+    closeButton.classList.add('close');
+    closeButton.innerHTML = 'X';
 
-  // Create the iframe
-  const iframe = document.createElement('iframe');
-  iframe.setAttribute('src', videoUrl);
-  iframe.setAttribute('allowfullscreen', '');
+    // Create the iframe
+    const iframe = document.createElement('iframe');
+    iframe.setAttribute('src', videoUrl);
+    iframe.setAttribute('allowfullscreen', '');
 
-  // Add the iframe and close button to the modal
-  modal.appendChild(closeButton);
-  modal.appendChild(iframe);
+    // Add the iframe and close button to the modal
+    modal.appendChild(closeButton);
+    modal.appendChild(iframe);
 
-  // Add the modal to the page
-  document.body.appendChild(modal);
+    // Add the modal to the page
+    document.body.appendChild(modal);
 
-  // Add a click event listener to the close button
-  closeButton.addEventListener('click', () => {
-    // Remove the modal from the page
-    document.body.removeChild(modal);
+    // Add a click event listener to the close button
+    closeButton.addEventListener('click', () => {
+      // Remove the modal from the page
+      document.body.removeChild(modal);
+    });
   });
 });
-
-
